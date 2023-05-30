fed059febd84770d843d7f465e5a10dd882c4ba1
Ronnie King <kingro@merrimack.edu>
2023-05-29 20:11:58 -0500

    this commit adds the initial service to the repo

---
+++ ai-code-inspector-service/src/main/java/com/example/ai-code-inspector-service/AiCodeInspectorApplication.java
@@ -0,0 +1,13 @@
+package com.example.aicodeinspector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiCodeInspectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiCodeInspectorApplication.class, args);
+    }
+
+}
---
+++ ai-code-inspector-service/src/test/java/com/example/aicodeinspector/AiCodeInspectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aicodeinspector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiCodeInspectorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
