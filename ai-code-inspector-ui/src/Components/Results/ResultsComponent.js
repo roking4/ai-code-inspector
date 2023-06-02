b7070f4957d16ceea553239c6cb22fc71cac910d
Ronnie King <kingro@merrimack.edu>
2023-06-01 20:14:26 -0500

    this commit adds dependency for the material icon and adds the copy icon along with the success and error messages

--- ai-code-inspector-ui/src/Components/Results/ResultsComponent.js
+++ ai-code-inspector-ui/src/Components/Results/ResultsComponent.js
@@ -1,6 +1,21 @@
+import Alert from '@mui/material/Alert';
+import AlertTitle from '@mui/material/AlertTitle';
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+
 function ResultsComponent(){
     return(
-        <div>hello</div>
+        <div>
+            <p>Code Placeholder</p>
+            <ContentCopyIcon />
+            <Alert severity="error">
+                <AlertTitle>Error</AlertTitle>
+                <strong>1 test failed!</strong>
+            </Alert>
+            <Alert severity="success">
+                <AlertTitle>Success</AlertTitle>
+                <strong>All 10 scenarios pass!</strong>
+            </Alert>
+        </div>
     );
 }
 
