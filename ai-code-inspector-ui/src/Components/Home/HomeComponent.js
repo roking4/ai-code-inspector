e48a6410a9f231550cd1184b6b05c16d54650bd6
Ronnie King <kingro@merrimack.edu>
2023-06-02 09:22:56 -0500

    this commit adds changing values of inputs and adding the values to state changes

--- ai-code-inspector-ui/src/Components/Home/HomeComponent.js
+++ ai-code-inspector-ui/src/Components/Home/HomeComponent.js
@@ -10,33 +10,101 @@ function HomeComponent() {
     const [scenarios, setScenarios] = useState([
         {
             index: 0,
-            numberOfInputs: [1],
-            numberOfOutputs: [1]
+            inputs: [""],
+            outputs: [""]
         }
     ]);
 
-    const handlePromptChange = (e) =>{
+    const handlePromptChange = (e) => {
         setFormPropmt(e.target.value);
-    }
+    };
+
+    const handleInputsChange = (e) => {
+        e.preventDefault();
+        const newValue = e.target.value;
+        const splitString = e.target.name.split("-");
+        const scenarioIndex = parseInt(splitString[0]);
+        const inputIndex = parseInt(splitString[1]);
+        changeInputValue(scenarioIndex, inputIndex, newValue);
+    };
+
+    const changeInputValue = (scenarioIndex, inputIndex, newValue) => {
+        const oldInputsArray = scenarios[scenarioIndex].inputs;
+        const newInputsArray = oldInputsArray.map((input) => {
+            if(oldInputsArray.indexOf(input) === inputIndex){
+                return newValue
+            }
+            else{
+                return input;
+            }
+        });
+        const newScenarios = scenarios.map((scenario) => {
+            if (scenario.index === scenarioIndex){
+                return {
+                    index: scenario.index,
+                    inputs: newInputsArray,
+                    outputs: scenario.outputs
+                }
+            }
+            else{
+                return scenario;
+            }
+        });
+        setScenarios(newScenarios);
+    };
+
+    const handleOutputsChange = (e) => {
+        e.preventDefault();
+        const newValue = e.target.value;
+        const splitString = e.target.name.split("-");
+        const scenarioIndex = parseInt(splitString[0]);
+        const outputIndex = parseInt(splitString[1]);
+        changeOutputValue(scenarioIndex, outputIndex, newValue);
+    };
+
+    const changeOutputValue = (scenarioIndex, outputIndex, newValue) => {
+        const oldInputsArray = scenarios[scenarioIndex].outputs;
+        const newOutputsArray = oldInputsArray.map((output) => {
+            if(oldInputsArray.indexOf(output) === outputIndex){
+                return newValue
+            }
+            else{
+                return output;
+            }
+        });
+        const newScenarios = scenarios.map((scenario) => {
+            if (scenario.index === scenarioIndex){
+                return {
+                    index: scenario.index,
+                    inputs: scenario.inputs,
+                    outputs: newOutputsArray
+                }
+            }
+            else{
+                return scenario;
+            }
+        });
+        setScenarios(newScenarios);
+    };
 
     const addScenario = () => {
         const newScenario = {
             index: scenarios.length,
-            numberOfInputs: [1],
-            numberOfOutputs: [1]
+            inputs: [""],
+            outputs: [""]
         };
         setScenarios([...scenarios, newScenario]);
-    }
+    };
 
-    const addInput = (index) => {
-        const oldNumberOfInputs = scenarios[index].numberOfInputs;
-        const newNumberOfInputs = [...oldNumberOfInputs, oldNumberOfInputs.length + 1];
+    const addInputField = (index) => {
+        const oldInputsArray = scenarios[index].inputs;
+        const newInputsArray = [...oldInputsArray, ""];
         const newScenarios = scenarios.map((scenario) => {
             if (scenario.index === index){
                 return {
                     index: scenario.index,
-                    numberOfInputs: newNumberOfInputs,
-                    numberOfOutputs: scenario.numberOfOutputs
+                    inputs: newInputsArray,
+                    outputs: scenario.outputs
                 }
             }
             else{
@@ -44,17 +112,17 @@ function HomeComponent() {
             }
         });
         setScenarios(newScenarios);
-    }
+    };
 
-    const addOutput = (index) => {
-        const oldNumberOfOutputs = scenarios[index].numberOfOutputs;
-        const newNumberOfOutputs = [...oldNumberOfOutputs, oldNumberOfOutputs.length + 1];
+    const addOutputField = (index) => {
+        const oldOutputsArray = scenarios[index].outputs;
+        const newOutputsArray = [...oldOutputsArray, ""];
         const newScenarios = scenarios.map((scenario) => {
             if (scenario.index === index){
                 return {
                     index: scenario.index,
-                    numberOfInputs: scenario.numberOfInputs,
-                    numberOfOutputs: newNumberOfOutputs
+                    inputs: scenario.inputs,
+                    outputs: newOutputsArray
                 }
             }
             else{
@@ -62,11 +130,11 @@ function HomeComponent() {
             }
         });
         setScenarios(newScenarios);
-    }
+    };
 
     const handleSubmit = (e) => {
 
-    }
+    };
 
     return (
         <div>
@@ -87,33 +155,39 @@ function HomeComponent() {
                             <h2 key={ "scenario-header" + scenario.index }>Test Scenario { scenario.index + 1 }</h2>
                             <div key={ "input-container" + scenario.index }>
                                 {
-                                    scenario.numberOfInputs.map((index) =>
-                                        <div key={ "input" + scenario.index + index } className={ "input" }>
+                                    scenario.inputs.map((input) =>
+                                        <div key={ "input" + scenario.index + scenario.inputs.indexOf(input) } className={ "input" }>
                                             <TextField
-                                                key={ "textField" + scenario.index + index }
+                                                key={ "textField" + scenario.index + scenario.inputs.indexOf(input) }
                                                 id="outlined-basic"
                                                 label="Expected Input"
                                                 variant="outlined"
+                                                value={ input }
+                                                name={ scenario.index + '-' + scenario.inputs.indexOf(input) }
+                                                onChange={ handleInputsChange }
                                             />
                                         </div>
                                     )
                                 }
-                                <Button key={ "add-button-input" + scenario.index } className={ "add-button" } onClick={ () => addInput(scenario.index) } variant="contained">+</Button>
+                                <Button key={ "add-button-input" + scenario.index } className={ "add-button" } onClick={ () => addInputField(scenario.index) } variant="contained">+</Button>
                             </div>
                             <div key={ "output-container" + scenario.index }>
                                 {
-                                    scenario.numberOfOutputs.map((index) =>
-                                        <div key={ "output" + scenario.index + index } className={ "input" }>
+                                    scenario.outputs.map((output) =>
+                                        <div key={ "output" + scenario.index + scenario.outputs.indexOf(output) } className={ "input" }>
                                             <TextField
-                                                key={ "textField" + scenario.index + index }
+                                                key={ "textField" + scenario.index + scenario.outputs.indexOf(output) }
                                                 id="outlined-basic"
                                                 label="Expected Output"
                                                 variant="outlined"
+                                                value={ output }
+                                                name={ scenario.index + '-' + scenario.outputs.indexOf(output) }
+                                                onChange={ handleOutputsChange }
                                             />
                                         </div>
                                     )
                                 }
-                                <Button key={ "add-button-output" + scenario.index } className={ "add-button" } onClick={ () => addOutput(scenario.index) } variant="contained">+</Button>
+                                <Button key={ "add-button-output" + scenario.index } className={ "add-button" } onClick={ () => addOutputField(scenario.index) } variant="contained">+</Button>
                             </div>
                         </div>
                     )
